3bedace23cfb21f9245b106ccbec1a4b69939c8a
ghpreeti <preetisinghitt@gamil.com>
2026-02-16 23:39:45 +0530

    practicing program

---
+++ SortedDictionaryProjectAssesment/PharmacyMedicineInventory/CustomException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyMedicineInventory
+{
+    // Invalid price exception
+    public class InvalidPriceException : Exception
+    {
+        public InvalidPriceException(string message) : base(message) { }
+    }
+
+    // Duplicate medicine exception
+    public class DuplicateMedicineException : Exception
+    {
+        public DuplicateMedicineException(string message) : base(message) { }
+    }
+
+    // Invalid expiry year exception
+    public class InvalidExpiryYearException : Exception
+    {
+        public InvalidExpiryYearException(string message) : base(message) { }
+    }
+
+    // Medicine not found exception
+    public class MedicineNotFoundException : Exception
+    {
+        public MedicineNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ SortedDictionaryProjectAssesment/PharmacyMedicineInventory/Medicine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyMedicineInventory
+{
+    public class Medicine
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int ExpiryYear { get; set; }
+    }
+}
